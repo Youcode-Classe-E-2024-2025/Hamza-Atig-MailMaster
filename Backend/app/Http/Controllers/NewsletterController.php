45500dfb836c2885ea55239f665605268575cba4
Atig-Hamza <hamzaatig@icloud.com>
2025-04-16 12:10:57 +0000

    make store function send email to subscribers

--- Backend/app/Http/Controllers/NewsletterController.php
+++ Backend/app/Http/Controllers/NewsletterController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Campaign;
 use App\Models\Newsletter;
+use App\Models\Subscriber;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Mail;
 
 class NewsletterController extends Controller
 {
@@ -25,12 +29,36 @@ class NewsletterController extends Controller
             'content' => 'required|string',
         ]);
 
+        $campaign = Campaign::latest()->first();
+
+        if (!$campaign) {
+            return response()->json([
+                'message' => 'No campaign available to use for sending the newsletter.'
+            ], 400);
+        }
+
         $newsletter = Newsletter::create([
             'title' => $request->title,
             'content' => $request->content,
         ]);
 
-        return response()->json(['message' => 'Newsletter created successfully', 'data' => $newsletter], 201);
+        $subscribers = Subscriber::all();
+
+        foreach ($subscribers as $subscriber) {
+            Mail::send('newsletter', [
+                'subject' => $campaign->subject,
+                'body' => $campaign->body,
+            ], function ($message) use ($subscriber, $campaign) {
+                $message->to($subscriber->email);
+                $message->subject($campaign->subject);
+                $message->from('hamzaatig58@gmail.com', 'MailMaster');
+            });
+        }
+
+        return response()->json([
+            'message' => 'Newsletter created and campaign sent to all subscribers.',
+            'data' => $newsletter
+        ], 201);
     }
 
     /**
